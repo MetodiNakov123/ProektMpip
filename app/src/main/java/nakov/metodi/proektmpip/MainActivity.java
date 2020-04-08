88cb6c48e9ecbf509c679f52f993ec1261ea260c
MetodiNakov123 <nakovmetodi181@gmail.com>
2020-04-09 01:40:18 +0200

    Created layout for User Login

--- app/src/main/java/nakov/metodi/proektmpip/MainActivity.java
+++ app/src/main/java/nakov/metodi/proektmpip/MainActivity.java
@@ -2,13 +2,32 @@ package nakov.metodi.proektmpip;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin;
+    Button btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.btnUserLogin);
+        btnSignup = findViewById(R.id.btnUserSignup);
+
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
